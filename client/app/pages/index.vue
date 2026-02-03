5c09be46399900d32ce2079dabf3b75c89f08253
safwan <safwanpaloli7@gmail.com>
2026-02-03 11:51:55 +0530

    Created: Server to handle Api's ,

--- client/app/pages/index.vue
+++ client/app/pages/index.vue
@@ -14,12 +14,12 @@
           <label for="password" class="block text-sm/6 font-medium text-gray-100">Password</label>
         </div>
         <div class="mt-2">
-          <input id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+          <input id="password" type="password" v-model="loginCredentials.password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
         </div>
       </div>
 
       <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign in</button>
+        <button type="submit" @click="login" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign in</button>
       </div>
     </div>
 
@@ -31,13 +31,8 @@
 </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
 
 const { login, isLoggedIn, loginCredentials } = useLogin()
-const isValidEmail = (emailStr: string) => {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-  return emailRegex.test(emailStr)
-}
 </script>
 
 <style scoped>
